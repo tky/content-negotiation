ac88a7e01b30b65d165f05de236e169cc33f41a3
taka- <takayuki.for.public@gmail.com>
2013-06-09 15:24:30 +0900

    added original ContentNegotiationViewResolver

--- src/main/java/com/sample/controller/JsonPViewResolver.java
+++ src/main/java/com/sample/controller/JsonPViewResolver.java
@@ -4,7 +4,6 @@ import java.util.Locale;
 
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
 
 public class JsonPViewResolver implements ViewResolver {
 
---
+++ src/main/java/com/sample/controller/LocalContentNegotiatingViewResolver.java
@@ -0,0 +1,28 @@
+package com.sample.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+
+public class LocalContentNegotiatingViewResolver extends ContentNegotiatingViewResolver {
+
+    // [application/javascript]
+    /**
+     * MediaTypeの判定を拡張します。
+     * 
+     * callbackが指定された場合はjsonpにします。
+     */
+    @Override
+    protected List<MediaType> getMediaTypes(HttpServletRequest request) {
+        if (!StringUtils.isEmpty(request.getParameter("callback"))) {
+            return Arrays.asList(new MediaType("application", "javascript"));
+        } else {
+            return super.getMediaTypes(request);
+        }
+    }
+}
